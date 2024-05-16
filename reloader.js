8cd4ec953822e2caa21db68b92c4b02f9377266b
Linkinlog <dahlton@dahlton.org>
2024-05-16 09:27:17 -0400

    feat: create reloader module

---
+++ reloader.js
@@ -0,0 +1,84 @@
+export const Reloader = function () {
+  let counter = 0;
+  const warningId =
+    "warning-" + Math.round((Math.floor(Math.random() * 1000) + 0) / 10) * 10;
+  const warningText =
+    "Page will reload shortly due to inactivity, resume activity to cancel.";
+
+  function init(
+    countTillWarn = 30,
+    countTillReload = 60 * 5,
+    countIntervalMs = 1000,
+    warningMsg = "",
+  ) {
+    if (warningMsg) {
+      warningText = warningMsg;
+    }
+    establishEvents();
+
+    execOnInterval(countIntervalMs, () => {
+      checkCounter(countTillReload, countTillWarn);
+      counter++;
+    });
+  }
+
+  function execOnInterval(interval, callback) {
+    callback();
+    setTimeout(() => {
+      execOnInterval(interval, callback);
+    }, interval);
+  }
+
+  function checkCounter(timelimit, warning) {
+    if (timelimit < warning) {
+      throw new Error(
+        "Time limit must be greater than or equal to warning time",
+      );
+    }
+
+    if (counter >= warning) {
+      displayWarning(warningText);
+    }
+    if (counter >= timelimit) {
+      location.reload();
+    }
+  }
+
+  function establishEvents() {
+    window.addEventListener("mousemove", resetCounterAndClearWarn);
+    window.addEventListener("keypress", resetCounterAndClearWarn);
+    window.addEventListener("scroll", resetCounterAndClearWarn);
+    window.addEventListener("touchstart", resetCounterAndClearWarn);
+  }
+
+  function resetCounterAndClearWarn() {
+    counter = 0;
+    clearWarning();
+  }
+
+  function clearWarning() {
+    const warning = document.getElementById(warningId);
+    if (warning) {
+      warning.remove();
+    }
+  }
+
+  function displayWarning(warningText) {
+    if (document.getElementById(warningId)) {
+      return;
+    }
+    const body = document.querySelector("body");
+    const warning = document.createElement("div");
+    warning.id = warningId;
+    const warningP = document.createElement("p");
+    warningP.innerText = warningText;
+    warning.appendChild(warningP);
+    body.appendChild(warning);
+  }
+
+  return {
+    init: init,
+  };
+};
+
+export default Reloader;
